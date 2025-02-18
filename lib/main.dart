69f5f97c65722b06360b2179185cfeb3617e6946
https://github.com/LamiaLiza991 <lamializa991@gmail.com>
2025-02-18 11:17:43 +0600

    icon

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,12 @@ class LabClass05 extends StatelessWidget {
         ),
         body: Row(
           children: [
-            Text(
+            Icon(
+              Icons.ac_unit,
+              size: 60,
+              color: const Color.fromARGB(255, 0, 217, 255),
+             ),
+             Text(
               "Hi, ",
               style: TextStyle(
                 color: Colors.red,
@@ -32,8 +37,7 @@ class LabClass05 extends StatelessWidget {
               style: TextStyle(
                 color: Colors.blue,
                 fontSize: 30,
-              ),
-            ),
+              )),
             Text(
               "Flutter, ",
               style: TextStyle(
@@ -44,58 +48,6 @@ class LabClass05 extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-     
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-   
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-       
-        child: Column(
-         
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
     );
   }
 }
