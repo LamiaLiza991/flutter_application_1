b92d56fbc3fe620d3724e51fffb195a4f1576de8
https://github.com/LamiaLiza991 <lamializa991@gmail.com>
2025-02-18 12:11:30 +0600

    singel screen veiw

--- lib/main.dart
+++ lib/main.dart
@@ -17,38 +17,25 @@ class LabClass05 extends StatelessWidget {
           backgroundColor: Colors.blue[400],
           centerTitle: true,
         ),
-        body: Container(
-          height: double.infinity,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center, 
-            children: [
-              Icon(
-                Icons.ac_unit,
-                size: 60,
-                color: const Color.fromARGB(255, 0, 217, 255),
+         body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal, 
+          child: Container(
+            height: 600,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly, 
+              crossAxisAlignment: CrossAxisAlignment.center, 
+              children: List.generate(
+                100,
+                (index) => Icon(
+                  Icons.airplanemode_active, // Using standard Flutter icon
+                  size: index.toDouble(), 
+                  color: const Color.fromARGB(255, 21, 0, 255),
+                ),
               ),
-              Icon(
-                Icons.ac_unit,
-                size: 60,
-                color: const Color.fromARGB(255, 240, 86, 9),
-              ),
-              Icon(
-                Icons.ac_unit,
-                size: 60,
-                color: const Color.fromARGB(255, 6, 48, 237),
-              ),
-              Icon(
-                Icons.ac_unit,
-                size: 60,
-                color: const Color.fromARGB(255, 228, 70, 243),
-              ),
-            ],
+            ),
           ),
-        )
-          
         ),
-      
+      ),
     );
   }
 }
